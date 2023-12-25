db7ce9462fdfd3c08deca4fdbbc8ea8bb90536ab
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2023-12-25 14:46:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function submit(){
+    let input=document.getElementById("input").value
+    let inputsplit=input.split("");
+
+    if(isNaN(parseInt(input))){
+        checkthala(input,inputsplit.length)
+    }else{
+        let sum=0;
+        for(let i=0;i<inputsplit.length;i++)
+            sum+=parseInt(inputsplit[i])
+        
+        checkthala(input,sum)
+    }
+    
+
+   function checkthala(input,length){
+    let result=document.getElementById("result")
+    if(length===7){
+       result.innerText=(input+" is thala for a reason")
+       result.style.display="block"
+    }else{
+        result.innerText=(input+" is not thala for a reason")
+       result.style.display="block"
+    }
+   }
+}
