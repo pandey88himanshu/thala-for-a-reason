7457e6e310c28ad388538bcfe802b392833f4ad0
pandey88himanshu <pandey88himanshu@gmail.com>
2024-01-03 12:56:56 +0530

    video logic

--- main.js
+++ main.js
@@ -22,24 +22,30 @@ function submit(){
    function checkthala(input,length){
     let result=document.getElementById("result")
     let video = document.getElementById("myVideo")
+    let video1 = document.getElementById("myVideo1")
     
     if(length===7){
        result.innerText=(input+" is thala for a reason")
        result.style.display="block"
        video.style.display="inline"
+       video1.style.display="none"
        t1.classList.toggle("hide")
        t2.classList.toggle("hide")
        t3.classList.toggle('view')
        video.volume=0.1
        video.play()
+       video1.pause()
     }else{
         result.innerText=(input+" is not thala for a reason")
        result.style.display="block"
        video.style.display="none"
+       video1.style.display="inline"
        t1.classList.toggle("hide")
        t2.classList.toggle("hide")
        t3.classList.toggle('view')
        video.pause()
+       video1.volume=0.1
+       video1.play()
     }
    }
 }
